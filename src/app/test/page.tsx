dfee062d57e19309803eb5378507b8ebc6fb6c96
Kendoo41 <daominhkhoi41@gmail.com>
2025-06-25 23:05:03 +0700

    Push

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+const LoginPage = () => {
+  return (
+    //<div className='flex flex-col'>
+    <div className='flex'>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  )
+}
+
+export default LoginPage
