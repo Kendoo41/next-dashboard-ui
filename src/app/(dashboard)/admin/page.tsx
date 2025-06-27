48212f3d784a61949f20e8dbced31d3adac0d072
Kendoo41 <daominhkhoi41@gmail.com>
2025-06-27 21:59:18 +0700

    Push

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,12 +2,14 @@ import UserCard from "@/components/UserCard"
 import CountChart from "@/components/CountChart"
 import AttendanceChart from "@/components/AttendanceChart"
 import FinanceChart from "@/components/FinanceChart"
+import EventCalendar from "@/components/EventCalendar"
+import Announcements from "@/components/Announcements"
 
 const AdminPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/* LEFT */}
-      <div className='w-full lg:w-2/3 flex flex-col gap-8'>Left
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type='student'></UserCard>
           <UserCard type='teacher'></UserCard>
@@ -31,8 +33,14 @@ const AdminPage = () => {
         </div>
       </div>
 
+      
+
+
       {/* RIGHT  */}
-      <div className="w-full lg:w-1/3">Right</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar></EventCalendar>
+        <Announcements></Announcements>
+      </div>
 
     </div>
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -117,6 +117,31 @@ const menuItems = [
   },
 ];
 
+// const Menu = () => {
+//   return (
+//     <div className="mt-4 text-sm">
+//       {menuItems.map(i => (
+//         <div className="flex flex-col gap-2" key={i.title}>
+//           <span className="hidden lg:block text-gray-400 font-light my-4">
+//             {i.title}
+//           </span>
+//           {i.items.map( item => (
+//             <Link 
+//               href={item.href} 
+//               key={item.label} 
+//               className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+//             >
+//               <Image src={item.icon} alt="" width={20} height={20}/>
+//               <span className="hidden lg:block">{item.label}</span>
+//             </Link>
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+const role = 'student';
 const Menu = () => {
   return (
     <div className="mt-4 text-sm">
@@ -125,16 +150,20 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map( item => (
-            <Link 
-              href={item.href} 
-              key={item.label} 
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={item.icon} alt="" width={20} height={20}/>
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link 
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20}></Image>
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+            }
+          })}
         </div>
       ))}
     </div>
